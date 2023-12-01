98e612c2ba3fa22dfb354b7cafb4477a363eb37d
Max Rothman <max.r.rothman@gmail.com>
2023-12-01 10:36:38 -0500

    day 1

--- 2023/clj2023/src/clj2023/core.clj
+++
@@ -1,7 +0,0 @@
-(ns clj2023.core
-  (:gen-class))
-
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
---
+++ 2023/clj2023/src/clj2023/day1.clj
@@ -0,0 +1,73 @@
+(ns clj2023.day1
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]))
+
+(defn parse []
+  (->> (io/resource "day1.txt")
+       io/reader
+       line-seq))
+
+(defn first-digit [s]
+  (->> s
+       (re-seq #"[^\d]*(\d)")
+       first second))
+
+(defn last-digit [s]
+  (->> s
+       (re-seq #".*(\d)[^\d]*$")
+       first second))
+
+(defn pt1 []
+  (->> (parse)
+       (map (juxt first-digit last-digit))
+       (map str/join)
+       (map parse-long)
+       (reduce +)))
+
+(comment
+  (pt1)
+  ;; => 55477
+  )
+
+(def nums
+  {"one" "1"
+   "two" "2"
+   "three" "3"
+   "four" "4"
+   "five" "5"
+   "six" "6"
+   "seven" "7"
+   "eight" "8"
+   "nine" "9"})
+
+(def digit-re
+  (str "\\d|" (str/join "|" (keys nums))))
+
+(def first-re
+  (re-pattern (str "^(?:(?!" digit-re ").)*(" digit-re ")")))
+
+(def last-re
+  (re-pattern (str ".*(" digit-re ")(?:(?!" digit-re ").)*$")))
+
+(defn match [re s]
+  (->> s
+       (re-seq re)
+       first second))
+
+(defn num->digit [s]
+  (if (re-matches #"\d" s)
+    s
+    (nums s)))
+
+(defn pt2 []
+  (->> (parse)
+       (map (juxt (partial match first-re) (partial match last-re)))
+       (map #(mapv num->digit %))
+       (map str/join)
+       (map parse-long)
+       (reduce +)))
+
+(comment
+  (pt2)
+  ;; => 54431
+  )
